e5c81dbe3751836e6edffad5c73604ed783725bb
Adam-smh <africa.adam1@gmail.com>
2021-08-25 20:17:39 +0200

    backend completed

---
+++ app.py
@@ -0,0 +1,511 @@
+# all imports
+# cloudinary used to store images in db
+import cloudinary
+import cloudinary.uploader
+
+# importing flask and jwt and CORS
+from flask import Flask, request
+from flask_jwt import JWT, jwt_required, current_identity
+from flask_cors import CORS
+from flask_mail import Mail, Message
+
+# other imports
+import hmac
+import sqlite3
+import datetime
+
+
+# User class
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+# Product class
+class Product(object):
+    def __init__(self, user_id, product_name, product_image, product_category, product_description, product_price):
+        self.user_id = user_id
+        self.product_name = product_name
+        self.product_image = product_image
+        self.product_category = product_category
+        self.product_description = product_description
+        self.product_price = product_price
+
+
+# Database class
+class Database(object):
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+        self.cursor = self.conn.cursor()
+
+    def registration(self, first_name, last_name, email, username, password):
+        # Sending user info to database
+        self.cursor.execute("INSERT INTO user("
+                            "first_name,"
+                            "last_name,"
+                            "email,"
+                            "username,"
+                            "password) VALUES(?, ?, ?, ?, ?)", (first_name, last_name, email, username, password))
+        self.conn.commit()
+
+    # Add product to database
+    def add_product(self, user_id, product_name, product_image, product_category, product_description, product_price):
+
+        # Upload image to cloudinary
+        cloudinary.config(cloud_name='ddvdj4vy6', api_key='416417923523248',
+                          api_secret='v_bGoSt-EgCYGO2wIkFKRERvqZ0')
+        upload_result = None
+
+        app.logger.info('%s file_to_upload', product_image)
+        if product_image:
+            upload_result = cloudinary.uploader.upload(product_image)  # Upload results
+            app.logger.info(upload_result)
+            # data = jsonify(upload_result)
+        self.cursor.execute("INSERT INTO product (user_id, product_name, product_image_url, product_category, "
+                            "product_description, product_price) VALUES (?, ?, ?, ?, ?, ?)",
+                            (user_id, product_name, upload_result['url'], product_category, product_description,
+                             product_price))
+
+        self.conn.commit()
+
+    # fetch products
+    def get_products(self):
+        self.cursor.execute("SELECT * FROM product")
+        return self.cursor.fetchall()
+
+    # fetch one specific product
+    def view_product(self, product_id):
+        self.cursor.execute('SELECT * FROM product WHERE product_id={}'.format(product_id))
+        return self.cursor.fetchone()
+
+    # edit product
+    def edit_product(self, product_data, product_id):
+        response = {}
+        put_data = {}
+
+        # if statements are to check if data received is not empty
+        if product_data.get('product_name'):
+            put_data['product_name'] = product_data.get('product_name')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_name=? WHERE product_id=?", (put_data["product_name"],
+                                                                                        product_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        #
+        if product_data.get('product_category'):
+            put_data['product_category'] = product_data.get('product_category')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_category=? WHERE product_id=?",
+                               (put_data["product_category"], product_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        if product_data.get('product_description'):
+            put_data['product_description'] = product_data.get('product_description')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_description=? WHERE product_id=?",
+                               (put_data["product_description"], product_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        if product_data.get('product_price'):
+            put_data['product_price'] = product_data.get('product_price')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_price=? WHERE product_id=?", (put_data["product_price"],
+                                                                                         product_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        return response
+
+    def edit_profile(self, user_data, user_id):
+        response = {}
+        put_data = {}
+
+        # if statements are to check if data received is not empty
+        if user_data.get('first_name'):
+            put_data['first_name'] = user_data.get('first_name')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE user SET first_name=? WHERE user_id=?", (put_data["first_name"], user_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        if user_data.get('last_name'):
+            put_data['last_name'] = user_data.get('last_name')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE user SET last_name=? WHERE user_id=?",
+                               (put_data["last_name"], user_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        if user_data.get('email'):
+            put_data['email'] = user_data.get('email')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE user SET email=? WHERE user_id=?",
+                               (put_data["email"], user_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        if user_data.get('username'):
+            put_data['username'] = user_data.get('username')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE user SET username=? WHERE user_id=?", (put_data["username"], user_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        if user_data.get('password'):
+            put_data['password'] = user_data.get('password')
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE user SET password=? WHERE user_id=?", (put_data["password"], user_id))
+                conn.commit()
+                response['message'] = "Update was successful"
+                response['status_code'] = 200
+
+        return response
+
+    # Delete product
+    def delete_product(self, product_id):
+        with sqlite3.connect('database.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute('DELETE FROM product WHERE product_id={}'.format(product_id))
+            conn.commit()
+
+
+# create user table
+def init_user_table():
+    conn = sqlite3.connect('database.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "first_name TEXT NOT NULL,"
+                 "last_name TEXT NOT NULL,"
+                 "email TEXT NOT NULL,"
+                 "username TEXT NOT NULL,"
+                 "password TEXT NOT NULL)")
+    print("user table created successfully")
+    conn.close()
+
+
+# Create product table
+def init_product_table():
+    conn = sqlite3.connect('database.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS product(product_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "user_id TEXT,"
+                 "product_name TEXT NOT NULL,"
+                 "product_image_url TEXT NOT NULL,"
+                 "product_category TEXT NOT NULL,"
+                 "product_description TEXT NOT NULL,"
+                 "product_price TEXT NOT NULL,"
+                 "FOREIGN KEY (user_id) "
+                 "REFERENCES user(user_id))")
+    print('product table created successfully')
+    conn.close()
+
+
+init_user_table()
+init_product_table()
+
+
+# fetch users for JWT
+def fetch_users():
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user")
+        users = cursor.fetchall()
+
+        new_data = []
+
+        for data in users:
+            new_data.append(User(data[0], data[4], data[5]))
+    return new_data
+
+
+def authenticate(username, password):
+    users = fetch_users()
+    username_table = {u.username: u for u in users}
+    user = username_table.get(username, None)
+    if user and hmac.compare_digest(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    users = fetch_users()
+    userid_table = {u.id: u for u in users}
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+
+# Initialise app
+app = Flask(__name__)
+app.debug = True
+
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(hours=24)  # Extending token expiration
+
+# Mail config
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = "adamafrica.dev@gmail.com"
+app.config['MAIL_PASSWORD'] = "TaxiDriver8"
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+# mail instantiation
+mail = Mail(app)
+
+jwt = JWT(app, authenticate, identity)
+
+CORS(app)
+
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+
+# user functions
+# Registration
+@app.route('/register/', methods=['POST'])
+def registration():
+    response = {}
+
+    if request.method == "POST":
+
+        # fetch form info
+        first_name = request.json['first_name']
+        last_name = request.json['last_name']
+        email = request.json['email']
+        username = request.json['username']
+        password = request.json['password']
+
+        # making sure all fields are filled
+        if not first_name or not last_name or not username or not password or not email:
+            response['message'] = 'One or more entries are empty'
+            response['status_code'] = 400
+            return response
+
+        # Check if details already exist
+        with sqlite3.connect('database.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM user WHERE username='{}'".format(username))
+            username_result = cursor.fetchone()
+
+            cursor.execute("SELECT * FROM user WHERE email='{}'".format(email))
+            email_result = cursor.fetchone()
+
+            if username_result:
+                response['message'] = 'Username already exists'
+                response['status_code'] = 400
+                return response
+
+            elif email_result:
+                response['message'] = 'Email already exists'
+                response['status_code'] = 400
+                return response
+
+        db = Database()
+        db.registration(first_name, last_name, email, username, password)
+
+        users = fetch_users()
+        username_table = {u.username: u for u in users}
+        userid_table = {u.id: u for u in users}
+
+        # Send email
+        msg = Message('Erispe8 Registration Successful', sender='adamafrica.dev@gmail.com', recipients=[email])
+        msg.body = "Hi there {}, \n \n" \
+                   "We are happy to have you on board. \n " \
+                   "Purchases will be received with an emailed receipt, " \
+                   "free shipping for orders over R450. \n \n" \
+                   "Happy shopping! \n" \
+                   "Erispe8".format(username)
+        mail.send(msg)
+
+        response["message"] = "success"
+        response["status_code"] = 201
+        return response
+
+
+# get user function
+@app.route('/get-user/<username>/')
+def get_user(username):
+    response = {}
+
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user where username='{}'".format(username))
+
+        response['status_code'] = 200
+        response['message'] = 'User retrieved successfully'
+        response['user'] = cursor.fetchone()
+
+    return response
+
+
+# edit user profile
+@app.route('/edit-profile/<int:user_id>', methods=['PUT'])
+@jwt_required()
+def edit_profile(user_id):
+    response = None
+
+    if request.method == 'PUT':
+        incoming_data = dict(request.json)
+        db = Database()
+        response = db.edit_profile(incoming_data, user_id)
+
+        return response
+
+
+@app.route('/delete-user/<int:user_id>/')
+@jwt_required()
+def delete_user_products(user_id):
+    response = {}
+
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE from user WHERE user_id='{}'".format(user_id))
+        conn.commit()
+
+        cursor.execute("DELETE from user WHERE user_id='{}'".format(user_id))
+        conn.commit()
+
+        response['status_code'] = 200
+        response['message'] = 'User deleted successfully'
+
+    return response
+
+
+@app.route('/get-user-products/<int:user_id>')
+@jwt_required()
+def get_user_products(user_id):
+    response = {}
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM product WHERE user_id='{}'".format(user_id))
+        response['status_code'] = 200
+        response['message'] = 'User products retrieved successfully'
+        response['products'] = cursor.fetchall()
+
+    return response
+
+
+# product manipulation functions
+@app.route('/add-product/<int:user_id>', methods=['POST'])
+@jwt_required()
+def add_product(user_id):
+    response = {}
+
+    if request.method == 'POST':
+        # error trapping
+        try:
+            # form data used to make adding images easier
+            product_name = request.form['product_name']
+            product_image = request.files['product_image']
+            product_category = request.form['product_category']
+            product_description = request.form['product_description']
+            product_price = request.form['product_price']
+
+            # checking if any field is left empty
+            if not product_name or not product_category or not product_description or not product_price:
+                response['message'] = 'One or more entries are empty'
+                response['status_code'] = 400
+                return response
+
+            # check if price is a number
+            int(product_price)
+
+            # pushing new product to db
+            db = Database()
+            db.add_product(user_id, product_name, product_image, product_category, product_description, product_price)
+
+            response['message'] = "Product entered successfully"
+            response['status_code'] = 200
+
+            return response
+
+        except ValueError:
+            response['message'] = "Price must be integer"
+            response['status_code'] = 400
+
+            return response
+
+
+@app.route('/show-products/')
+@jwt_required()
+def fetch_products():
+    response = {}
+
+    db = Database()
+
+    response['products'] = db.get_products()  # fetch products
+    response['message'] = 'Products retrieved successfully'
+    response['status_code'] = 200
+
+    return response
+
+
+@app.route('/view-product/<int:product_id>/')
+def view_product(product_id):
+    response = {}
+
+    db = Database()
+    product = db.view_product(product_id)  # fetch specific product with product id
+
+    response['message'] = 'Successfully retrieved product info'
+    response['status_code'] = 200
+    response['product'] = product
+
+    return response
+
+
+@app.route('/edit-product/<int:product_id>/', methods=['PUT'])
+@jwt_required()
+def edit_product(product_id):
+    response = None
+
+    if request.method == 'PUT':
+        incoming_data = dict(request.json)
+        db = Database()
+        response = db.edit_product(incoming_data, product_id)
+
+    return response
+
+
+@app.route('/delete-product/<int:product_id>/')
+@jwt_required()
+def delete_product(product_id):
+    response = {}
+
+    db = Database()
+    db.delete_product(product_id)
+
+    response['message'] = 'Product deleted successfully'
+    response['status_code'] = 200
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
